43cbd1088eba18a5780362d85f80ede4b01760ec
Vaikan Peddi <vaikanpeddi14@gmail.com>
2023-09-29 20:08:42 +0530

    Create oopsTwo.cpp

---
+++ 100 DOC/DAY#3/oopsTwo.cpp
@@ -0,0 +1,19 @@
+/*
+
+@Author: Vaikan Peddi
+@Date: 29th September 2023
+@Description: In this code, we will specifically be looking at the 
+              OOPS concepts of inheritance, polymorphism, and other C++ specific
+              objects that help us have a seamless Object-Oriented journey
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    return 0;
+}
