8d93209e3cbef98c6619e97aa3a0161a57273b8f
Vaikan Peddi <vaikanpeddi14@gmail.com>
2023-10-08 15:32:23 +0530

    Create bst.c

---
+++ 100 DOC/DAY#12/bst.c
@@ -0,0 +1,21 @@
+/*
+* Author: Vaikan Peddi
+* Date: 8th October 2023
+* Description: Binary Search Tree and its functions like finding minimum, diaplying elements, stc
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int key;
+    struct Node* left;
+    struct Node* right;
+} node;
+
+void add(node*, int);
+void inorder()
+
+int main(void) {
+
+}
